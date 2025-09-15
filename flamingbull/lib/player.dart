0ef8e649f6d6a94a6d6d3fa3cf9c62e24293d4eb
Adam Lenarduzzi <adamlenarduzzi@Adams-MacBook-Air.local>
2025-09-14 20:19:06 -0500

    added simple drop down logic for image plane to add sprite animations soon

--- flamingbull/lib/player.dart
+++ flamingbull/lib/player.dart
@@ -5,11 +5,24 @@ import 'package:flamingbull/constants.dart';
 
 class Player extends SpriteComponent {
 
-@override
+  @override
   FutureOr<void> onLoad() async {
     sprite = await Sprite.load('chicken.png');
-    size = Vector2.all(100);
+    size = Vector2.all(600);
     position = Vector2(0, -(gameHeight / 2) + (size.y / 2));
     anchor = Anchor.center;
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    double newY = position.y + (dt * 200);
+
+    if (newY > (gameHeight / 2) - (size.y / 2)) {
+      newY = (gameHeight / 2) - (size.y / 2);
+    }
+
+    position.y = newY;
   }
 }
